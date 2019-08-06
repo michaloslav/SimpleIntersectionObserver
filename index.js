bf2408f6435b7aa3d637b8ac71b0fc3f0ab14ef6
MichaelFarnik <michaelfarnik@gmail.com>
2019-08-06 18:26:46 +0200

    Compatibility fix

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ exports.simpleIntersectionObserver = function(elements, callback, options = {}){
   elements = [...elements] // IE compatibility
 
   elements.forEach(function(target){
-    const io = new IntersectionObserver(function(entries, observer){
-      entries.forEach(entry => {
+    var io = new IntersectionObserver(function(entries, observer){
+      entries.forEach(function(entry){
         if(entry.isIntersecting){
-          const element = entry.target
+          var element = entry.target
   
           callback(element)
   
